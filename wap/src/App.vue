6af6bf5bb284f997dba25dfe50731a5028f9f8c7
Uku Ilves <uku.ilves@ut.ee>
2022-10-29 22:08:33 +0300

    trying to dynamically create posts from json data

---
+++ wap/src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import Postitus from './components/Postitus.vue'
+
+</script>
+
+<script>
+const posts = await fetch("./public/data.json").then(res => res.json());
+console.log(posts);
+
+</script>
+
+<template>
+  <div class="flex-container flex-column">
+    <header>
+      <nav>
+        <div class="profile-picture">
+          <img class="logo" src="user.png" width="64" height="64" alt="" />
+        </div>
+        <ul class="linkcontainer">
+          <li><a class="active" href="#">Home</a></li>
+          <li><a href="addPOST.html">addPost</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <div class="flex-container">
+      <aside></aside>
+      <main class="flex-container flex-column justify-space">
+        <Postitus v-for="post in posts" :id="post.post_id" :text="post.post_text" :date="post.year" :contentImg="null"
+          :profileImg="post.post_profile_img" />
+      </main>
+      <aside></aside>
+    </div>
+
+    <footer>
+      <p id="para1">footer area</p>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ wap/src/components/Postitus.vue
@@ -0,0 +1,44 @@
+<script setup>
+defineProps({
+    id: {
+        type: Number,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    contentImg: {
+        type: String,
+        required: true
+    },
+    profileImg: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <article>
+        <section class="article-details">
+            <img :src="profileImg" width="28" height="28" alt="" />
+            <span>{{ date }}</span>
+        </section>
+        <section class="article-content">
+            <img :src="contentImg" alt="">
+            <p>{{ text }}</p>
+        </section>
+        <section class="article-footer">
+            <img src="like.png" width="64" height="64" alt="">
+        </section>
+    </article>
+</template>
+
+<style scoped>
+
+</style>
