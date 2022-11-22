e1a5887ce4161a7f683ef8474434043c0213609f
kristjanlaid <kristjan.laid98@gmail.com>
2022-11-22 22:45:47 +0200

    Added ssignup page

--- wad/src/router/index.js
+++ wad/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/home.vue'
+import Signup from "@/views/signup.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/login.vue')
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: Signup
     },
     {
       path: '/addPOST',
---
+++ wad/src/views/signup.vue
@@ -0,0 +1,59 @@
+<template>
+<div class="container">
+  <form class="signup-form" @submit.prevent="onSignup">
+    <div class="signup-row">
+      <label class="signup-label">Email</label>
+      <input type="text" class="signup-input" placeholder="Email" v-model="email">
+      <div class="error" v-if="errors.email">
+        {{errors.email}}
+      </div>
+    </div>
+    <div class="signup-row">
+      <label class="signup-label">Password</label>
+      <input type="password" class="signup-input" placeholder="Password" v-model="password">
+      <div class="error" v-if="errors.password">
+        {{errors.password}}
+      </div>
+    </div>
+
+    <div class="signup-submit">
+      <button type="submit">Signup</button>
+    </div>
+  </form>
+</div>
+</template>
+
+<script>
+import Validation from "@/services/Validation";
+import router from "@/router";
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      errors: [],
+    };
+  },
+  methods: {
+    onSignup() {
+      let valids = new Validation(this.email, this.password);
+
+      this.errors = valids.checkValid();
+
+      if (this.errors.length) {
+        return false;
+
+      }
+    },
+    goToHomePage() {
+      return router.push({name: 'home'});
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
